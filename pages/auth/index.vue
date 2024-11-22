5f52119b7d5fd16fdf0d8f916b749ef3c6f781f9
direction-hhha <direction.hhha@gmail.com>
2024-11-22 16:43:57 +0900

    로그인 및 게시판 페이지 추가 - 링크 테스트용

---
+++ pages/auth/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex flex-col min-h-screen bg-blue">
+        <h1>로그인</h1>
+    </div>
+</template>
---
+++ pages/board/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex flex-col min-h-screen bg-blue">
+        <h1>게시판</h1>
+    </div>
+</template>
