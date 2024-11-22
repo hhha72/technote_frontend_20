f46cff977e3c17af2798db6f73ddb01c1e477018
direction-hhha <direction.hhha@gmail.com>
2024-11-22 16:41:32 +0900

    오류사항 수정 및 정리

--- components/layout/AppHeader.vue
+++ components/layout/AppHeader.vue
@@ -2,25 +2,27 @@
     <header class="bg-gray-800 text-white p-4">
         <div class="container mx-auto flex justify-between items-center">
             <button @click="$emit('toggle-sidebar')" class="text-2xl">☰</button>
-            <h1 class="text-xl font-bold">게시판 관리</h1>
+            <h1 class="flex flex-grow pl-2 text-xl font-bold">게시판 관리</h1>
             <nav>
                 <NuxtLink to="/" class="mx-2 hover:text-gray-300">홈</NuxtLink>
                 <NuxtLink to="/board" class="mx-2 hover:text-gray-300">게시판</NuxtLink>
-                <NuxtLink v-if="!isAuthenticated" to="/login" class="mx-2 hover:text-gray-300">로그인</NuxtLink>
+                <NuxtLink v-if="!isAuthenticated" to="/auth" class="mx-2 hover:text-gray-300">로그인</NuxtLink>
                 <a v-else @click="logout" href="#" class="mx-2 hover:text-gray-300">로그아웃</a>
             </nav>
         </div>
     </header>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { useAuthStore } from '~/stores/auth'
+import { useAuthApi } from '~/stores/api/auth';
 
 const authStore = useAuthStore()
+const authApi = useAuthApi()
 const isAuthenticated = computed(() => authStore.isAuthenticated)
 
 const logout = () => {
-    authStore.logout()
+    authApi.logout()
     navigateTo('/')
 }
 </script>
