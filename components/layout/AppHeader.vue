68cb860e24a7820f62a16fce1450eefbd32ff087
direction-hhha <direction.hhha@gmail.com>
2024-11-22 17:57:12 +0900

    앱바 레이아웃 정리

--- components/layout/AppHeader.vue
+++ components/layout/AppHeader.vue
@@ -1,11 +1,11 @@
 <template>
     <header class="bg-gray-800 text-white p-4">
-        <div class="container mx-auto flex justify-between items-center">
+        <div class="my-auto flex flex-0 justify-between items-center">
             <button @click="$emit('toggle-sidebar')" class="text-2xl">☰</button>
             <h1 class="flex flex-grow pl-2 text-xl font-bold">게시판 관리</h1>
-            <nav>
-                <NuxtLink to="/" class="mx-2 hover:text-gray-300">홈</NuxtLink>
-                <NuxtLink to="/board" class="mx-2 hover:text-gray-300">게시판</NuxtLink>
+            <nav class="flex flex-0">
+                <!-- <NuxtLink to="/" class="mx-2 hover:text-gray-300">홈</NuxtLink>
+                <NuxtLink to="/board" class="mx-2 hover:text-gray-300">게시판</NuxtLink> -->
                 <NuxtLink v-if="!isAuthenticated" to="/auth" class="mx-2 hover:text-gray-300">로그인</NuxtLink>
                 <a v-else @click="logout" href="#" class="mx-2 hover:text-gray-300">로그아웃</a>
             </nav>
